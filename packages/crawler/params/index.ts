6cfc863a6150d644b0b0d40d3d58111097deddbe
yufengjie <2457522528@qq.com>
2024-11-27 21:44:04 +0800

    东城区爬完了,有机会再更新

--- packages/crawler/params/index.ts
+++ packages/crawler/params/index.ts
@@ -34,7 +34,7 @@ export const degree_map: M = {
 export const areabussiness_map: M = {
   key: 'areaBusiness',
   value: {
-    110101: '东城区',
+    // 110101: '东城区',
     110102: '西城区',
     110105: '朝阳区',
     110107: '石景山',
